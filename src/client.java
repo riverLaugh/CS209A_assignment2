210ad878d3ccf681998e954256f01ef32964fd1b
riverLaugh <2314398305@qq.com>
2023-11-22 12:08:13 +0800

    send success

--- src/client.java
+++ src/client.java
@@ -21,17 +21,17 @@ public class client {
             cmd = cmdList[0];
             try (ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
                 out.writeUTF(cmd);
+                System.out.println(cmd);
                 switch (cmd) {
                     case "up" -> {
-//                        out.writeUTF("ftp start");
                         for (int i = 1; i < cmdList.length; i++) {
-                            out.writeUTF("FILES_TP");
-                            String basePathString = "D:\\23f\\Java2\\CS209A_assignment2\\Upload";
+                            String basePathString = "D:\\23f\\java2\\assignment2\\Upload";
                             Path path = Paths.get(basePathString, cmdList[i]);
                             File file = path.toFile();
                             sendFile(file, out, basePathString);
                         }
                         out.writeUTF("END_OF_FILES");
+                        System.out.println("END_OF_FILES");
                     }
 
                 }
@@ -44,7 +44,9 @@ public class client {
     private static void sendSingleFile(File subfile, ObjectOutputStream out) throws FileNotFoundException, IOException {
         try (FileInputStream fileIn = new FileInputStream(subfile)) {
             out.writeUTF(subfile.getName());
+            System.out.printf("file name : %s\n", subfile.getName());
             out.writeLong(subfile.length());
+            System.out.printf("Long : %d\n", subfile.length());
             byte[] buffer = new byte[4096];
             int length;
             while ((length = fileIn.read(buffer)) > 0) {
@@ -53,9 +55,26 @@ public class client {
         }
     }
 
+    private static void sendDirFile(File subfile, ObjectOutputStream out) throws FileNotFoundException, IOException {
+        try (FileInputStream fileIn = new FileInputStream(subfile)) {
+            out.writeLong(subfile.length());
+            System.out.printf("Long : %d\n", subfile.length());
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = fileIn.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
+    }
+
+
     private static void sendFile(File file, ObjectOutputStream out, String basePathString) throws IOException {
         if (file.isDirectory()) {
+            System.out.println("it is a dir");
             out.writeBoolean(true);
+            System.out.println(true);
+            out.writeUTF("FILES_TP");
+            System.out.println("FILES_TP");
             try (Stream<Path> files = Files.walk(file.toPath());) {
                 Path basePath = Paths.get(basePathString);
                 files.forEach(path -> {
@@ -64,8 +83,9 @@ public class client {
                     if (!subfile.isDirectory()) { //只取文件
                         String relativePath = basePath.relativize(path).toString();
                         try {
-                            out.writeUTF(relativePath); // "1.txt"
-                            sendSingleFile(subfile, out);
+                            out.writeUTF(relativePath); // dir1/"1.txt"
+                            System.out.println(relativePath);
+                            sendDirFile(subfile, out);
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
@@ -73,9 +93,15 @@ public class client {
                 });
             }
             out.writeUTF("END_OF_DIR");
-        } else {
+        } else if (file.isFile()){
+            System.out.println("it is a file");
             out.writeBoolean(false);
+            out.writeUTF("FILES_TP");
+            System.out.println("FILES_TP");
             sendSingleFile(file, out);
+        }
+        else{
+            System.out.println(file.toPath().toString());
         }
     }
 
--- src/server.java
+++ src/server.java
@@ -14,9 +14,10 @@ public class server {
         System.out.println("Server is listening on port " + PORT);
 
         try {
+            Socket clientSocket = serverSocket.accept();
+            ClientHandler clientHandler = new ClientHandler(clientSocket);
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                pool.execute(new ClientHandler(clientSocket));
+                clientHandler.run();
             }
         } finally {
             serverSocket.close();
@@ -29,11 +30,25 @@ public class server {
         ClientHandler(Socket socket) {
             this.clientSocket = socket;
         }
-
-
-        private static void createFile(String relativePath, ObjectInputStream in) throws IOException {
-            Path path = Paths.get("D:\\23f\\Java2\\CS209A_assignment2\\Storage", relativePath);
+        private static void createDirFile(String relativePath, ObjectInputStream in) throws IOException {
+            Path path = Paths.get("D:\\23f\\java2\\assignment2\\Storage", relativePath);
+            System.out.printf("path : %s", path.toString());
             Files.createDirectories(path.getParent()); // 确保父目录存在
+
+            long fileLength = in.readLong(); // 读取文件长度
+            try (OutputStream fileOut = new FileOutputStream(path.toFile())) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while (fileLength > 0 && (bytesRead = in.read(buffer, 0, (int) Math.min(buffer.length, fileLength))) > 0) {
+                    fileOut.write(buffer, 0, bytesRead);
+                    fileLength -= bytesRead;
+                }
+            }
+        }
+
+        private static void createSingleFile( ObjectInputStream in) throws IOException {
+            Path path = Paths.get("D:\\23f\\java2\\assignment2\\Storage", in.readUTF());
+            System.out.printf("path : %s", path.toString());
 
             long fileLength = in.readLong(); // 读取文件长度
             try (OutputStream fileOut = new FileOutputStream(path.toFile())) {
@@ -50,27 +65,29 @@ public class server {
             try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
                  ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
                 String cmd = in.readUTF();
+                System.out.printf("cmd : %s\n", cmd);
                 switch (cmd) {
                     case "up" -> {
                         boolean isDirectory = in.readBoolean();
                         while (true) {
                             String key = in.readUTF();
+                            System.out.printf("key : %s\n", key);
                             if (key.equals("END_OF_FILES")) {
                                 break;
                             }
                             if (isDirectory) {
                                 while (true) {
                                     String relativePath = in.readUTF(); // 读取相对路径
+                                    System.out.println(relativePath);
                                     if ("END_OF_DIR".equals(relativePath)) {
                                         break; // 终止循环
                                     }
-                                    createFile(relativePath, in); // 创建文件
+                                    createDirFile(relativePath, in); // 创建文件
                                 }
                             } else {
-                                createFile("", in); // 创建文件
+                                createSingleFile( in); // 创建文件
                             }
                         }
-
                     }
                     case "down" -> {
 
